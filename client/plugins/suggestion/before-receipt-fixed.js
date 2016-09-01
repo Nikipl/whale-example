e5189c9745f9cab80bd98882d77eb58efb6f6c93
Denis Mikhaylov <notxcain@gmail.com>
2016-09-01 12:37:38 +0300

    Fix parameter name.
    View expects `suggestions`, not `products`

--- client/plugins/suggestion/before-receipt-fixed.js
+++ client/plugins/suggestion/before-receipt-fixed.js
@@ -4,14 +4,14 @@ var inventory = require('inventory')
 function handleMoment(context, navigation) {
   var receipt = context.receipt
   var suggestions = storage.get("receipt-suggestions-"+receipt.id)
-  var products =
+  var suggestedProducts =
     suggestions
     .map(productId => inventory.getProduct(productId))
     .filter(p => p.isAvailable)
 
   if (products.length > 0) {
-    navigation.pushView("suggestions", {
-      products: products,
+    navigation.pushView("suggestion-list", {
+      suggestions: suggestedProducts,
       receipt: receipt
     })
   } else {
